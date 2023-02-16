60aa1eb8f1b8d3ad592325a92dba0e3720c2c860
Nelis <neliseerdekens1@hotmail.com>
2023-02-16 21:20:32 +0100

    console log

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,16 @@ import { Inter } from "@next/font/google";
 // import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
+function jokes(): Promise<string> {
+  return fetch("https://api.chucknorris.io/jokes/random")
+    .then((data) => data.json())
+    .then((data) => data.toString());
+}
+console.log(jokes());
 export default function Home() {
   return (
     <>
-      <div>Hello</div>
+      <div>Test</div>
     </>
   );
 }
