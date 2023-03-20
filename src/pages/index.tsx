954980998fdbfc52cbe75054f631075e6fd6b97d
Nelis <neliseerdekens1@hotmail.com>
2023-03-20 23:49:26 +0100

    installed tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
+import type { NextPage } from "next";
+
 import styles from "@/styles/Home.module.css";
 
-import { useEffect, useState } from "react";
-
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <>
       <div>
         <Head>
           <title>Nelis Portfolio</title>
         </Head>
-        <h1>Something here</h1>
+        <h1 className="text-red-500">Something here</h1>
       </div>
     </>
   );
-}
+};
+export default Home;
