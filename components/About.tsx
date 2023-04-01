53ed72b1e7977296a03c2636fddf654fcfa58807
Nelis <neliseerdekens1@hotmail.com>
2023-04-01 23:13:06 +0200

    including sanity

--- components/About.tsx
+++ components/About.tsx
@@ -25,12 +25,7 @@ export default function About({}: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src="/../IMG-20200323-WA0024.jpg"
-        className="mb-20 md:mb-0 flex-shrink-0
-        w-56
-        h-56
-        rounded-full
-        object-cover
-        md:rounded-lg md:w-64 md:h95 xl:w-[500px] xl:h-[600px]"
+        className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h95 xl:w-[500px] xl:h-[600px]"
       />
       <div className="space-y-10 px-0 md:px-10">
         <h4 className="text-4xl font-semibold">
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -16,7 +16,7 @@ export default function Projects({}: Props) {
         Projects
       </h3>
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-mandatory z-20 scrollbar-thin scrollbar-track-teal-300/40 scrollbar-thumb-yellow-500/80">
-        {/* {projects.map((project, i) => {
+        {projects.map((project, i) => {
           <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p44 h-screen">
             <motion.img
               initial={{ y: -300, opacity: 0 }}
@@ -36,7 +36,7 @@ export default function Projects({}: Props) {
               <p className="text-lg text-center md:text-left"></p>
             </div>
           </div>;
-        })} */}
+        })}
       </div>
       <div className="w-full absolute top-[30%] bg-teal-300/10 h-[500px] -skew-y-12"></div>
     </motion.div>
