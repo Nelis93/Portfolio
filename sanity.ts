2dd4759ba8f1e91fac2e7c162807808feb23e388
Nelis <neliseerdekens1@hotmail.com>
2023-04-03 23:32:51 +0200

    put files in correct folder

---
+++ sanity.ts
@@ -0,0 +1,15 @@
+import { createClient } from "../Final_Project/portfolio/node_modules/next-sanity";
+import createImageUrlBuilder from "../Final_Project/portfolio/node_modules/@sanity/image-url";
+
+export const config = {
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "pdxb9c80",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2022-11-16",
+  useCdn: process.env.NODE_ENV === "production",
+};
+
+// set up the client for fetching data in the getProps page funcitons
+export const sanityClient = createClient(config);
+
+export const urlFor = (source: any) =>
+  createImageUrlBuilder(config).image(source);
