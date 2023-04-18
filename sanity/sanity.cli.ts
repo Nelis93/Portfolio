ca5ac4376f592f04b3b3d3f744cae93b62414a2a
Nelis <neliseerdekens1@hotmail.com>
2023-04-18 20:20:53 +0200

    mmssg

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'pdxb9c80',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/index.ts
@@ -0,0 +1,7 @@
+import skill from './skill'
+import experience from './experience'
+import social from './social'
+import project from './project'
+import pageInfo from './pageInfo'
+
+export const schemaTypes = [pageInfo, skill, project, experience, social]
