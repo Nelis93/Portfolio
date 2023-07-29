7a87b81654ce41d30dd2938b49557708844d16b6
Nelis <neliseerdekens1@hotmail.com>
2023-07-29 23:12:06 +0200

    placed sanity cofig back in sanity folder

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from './node_modules/@sanity/vision'
+
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Sanity',
+
+  projectId: 'pdxb9c80',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+// C:\Users\nelis\Documents\Syntra\Final_Project\portfolio\sanity\node_modules\@sanity\vision
