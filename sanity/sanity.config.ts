273aafcc512200a00579a5981a60810a6814bdc1
Nelis <neliseerdekens1@hotmail.com>
2023-08-15 23:35:10 +0200

    visionTool fix 2nd attempt

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'sanity/lib/exports'
 import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
+import {visionTool} from '@sanity/vision/src/visionTool'
 
 import {schemaTypes} from './schemas'
 
