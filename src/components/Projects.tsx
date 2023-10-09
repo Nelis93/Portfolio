17f35d366d62619a7b8806a1a3dc4c213f95ce22
Eerdekens Nelis (DRI) <nelis.eerdekens@police.belgium.eu>
2023-10-09 16:08:36 +0200

    changed z-index for projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,7 +18,7 @@ export default function Projects({ projects }: Props) {
       <h3 className="absolute top-[12vh] md:top-[7vh] uppercase tracking-[10px] xl:tracking-[20px] text-white text-[5vh]">
         Projects
       </h3>
-      <div className="relative w-screen h-screen px-10 xl:px-0 xl:pt-[20vh] flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-teal-300/40 scrollbar-thumb-yellow-500/80">
+      <div className="relative w-screen h-screen px-10 xl:px-0 xl:pt-[20vh] flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-0 scrollbar-thin scrollbar-track-teal-300/40 scrollbar-thumb-yellow-500/80">
         {projects?.map((project, i) => {
           return (
             <div
