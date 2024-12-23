3d14c7f0786c3e6c674a35dfd8fd831b3dacf9df
Nelis <neliseerdekens1@hotmail.com>
2024-12-23 02:47:08 +0100

    completed the filter component for the logbookentries

--- src/components/CaptainsLog/articleFilter.tsx
+++ src/components/CaptainsLog/articleFilter.tsx
@@ -1,65 +1,121 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+import { LogbookEntry } from "typings";
+import Link from "next/link";
+import { set } from "sanity";
 
-//take in logbookEntries
-const endpoint =
-  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+type ArticleFilterProps = {
+  logBookEntries: LogbookEntry[];
+};
 
-//cities should contain title and description
-const cities = [];
-fetch(endpoint)
-  .then((blob) => blob.json())
-  .then((data) => cities.push(...data));
-
-function findMatches(wordToMatch, cities) {
-  return cities.filter((place) => {
-    // here we need to figure out if the city or state matches what was searched
+export default function ArticleFilter({ logBookEntries }: ArticleFilterProps) {
+  const [query, setQuery] = useState("");
+  const [filteredEntries, setFilteredEntries] = useState<any[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  function findMatches(wordToMatch: string, entries: LogbookEntry[]) {
     const regex = new RegExp(wordToMatch, "gi");
-    return place.city.match(regex) || place.state.match(regex);
-  });
-}
+    return entries.filter(
+      (entry) => entry.title.match(regex) || entry.description.match(regex)
+    );
+  }
 
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    setQuery(value);
+    console.log(value);
+    const matches = findMatches(value, logBookEntries);
+    setFilteredEntries(matches);
+    !isOpen && setIsOpen(true);
+  }
 
-function displayMatches() {
-  const matchArray = findMatches(this.value, cities);
-  const html = matchArray
-    .map((place) => {
-      const regex = new RegExp(this.value, "gi");
-      const cityName = place.city.replace(
-        regex,
-        `<span class="hl">${this.value}</span>`
-      );
-      const stateName = place.state.replace(
-        regex,
-        `<span class="hl">${this.value}</span>`
-      );
-      return `
-<li>
-<span class="name">${cityName}, ${stateName}</span>
-<span class="population">${numberWithCommas(place.population)}</span>
-</li>
-`;
-    })
-    .join("");
-  suggestions.innerHTML = html;
-}
+  function handleKeyUp(event: React.KeyboardEvent<HTMLInputElement>) {
+    const value = (event.target as HTMLInputElement).value;
+    const matches = findMatches(value, logBookEntries);
+    setFilteredEntries(matches);
+  }
+  const handleClickOutside = (event: MouseEvent) => {
+    // If the clicked target is outside the dropdown, close it
+    //   console.log(dropdownRef.current);
+    //   event.stopPropagation();
+    //   if (
+    //     dropdownRef.current &&
+    //     !dropdownRef.current.contains(event.target as Node)
+    //   ) {
+    //     console.log("dropdown disengaged");
+    setIsOpen(false);
+  };
 
-const searchInput = document.querySelector(".search");
-const suggestions = document.querySelector(".suggestions");
+  useEffect(() => {
+    if (isOpen) {
+      // Add event listener to detect clicks outside the dropdown
+      console.log("dropdown engaged");
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      console.log("dropdown disengaged");
+      // Remove event listener when dropdown is closed
+      document.removeEventListener("click", handleClickOutside);
+    }
 
-searchInput.addEventListener("change", displayMatches);
-searchInput.addEventListener("keyup", displayMatches);
+    // Cleanup function to remove the event listener on unmount or state change
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen]);
 
-export default function articleFilter() {
   return (
     <div>
-      <form class="search-form">
-        <input type="text" class="search" placeholder="City or State" />
-        <ul class="suggestions">
-          <li>Filter for a city</li>
-          <li>or a state</li>
+      <form className="max-w-96 my-[50px]">
+        <input
+          type="text"
+          className="m-0 p-[20px] h-5 text-center text-2xl shadow-sm border-solid border-[10px] w-[120%] left-[-10%] relative top-[10px] z-20 border-[#F7F7F7] rounded-s"
+          placeholder="title or word"
+          value={query}
+          onChange={handleInputChange}
+          onKeyUp={handleKeyUp}
+          onClick={() => setIsOpen(true)}
+        />
+        <ul
+          className="relative m-0 p-0"
+          style={{ display: isOpen ? "block" : "none" }}
+        >
+          {filteredEntries.length > 0 ? (
+            filteredEntries.map((entry, index) => (
+              <Link
+                key={index}
+                href={`/captainsLog/bigLogs/${entry.slug.current}`}
+              >
+                <li
+                  key={index}
+                  className="bg-white hover:bg-stone-300 w-full list-none border-b-[1px] border-b-[#D8D8D8] m-0 p-[20px] transition-[background] flex flex-row flex-wrap justify-between capitalize"
+                >
+                  <span
+                    className="name text-2xl pb-3"
+                    dangerouslySetInnerHTML={{
+                      __html: entry.title.replace(
+                        new RegExp(`(${query})`, "gi"),
+                        (match: any) =>
+                          `<span class="bg-[#ffc600] w-full text-xl">${match}</span>`
+                      ),
+                    }}
+                  />
+                  <span
+                    className="description text-lg line-clamp-4"
+                    dangerouslySetInnerHTML={{
+                      __html: entry.description.replace(
+                        new RegExp(`(${query})`, "gi"),
+                        (match: any) =>
+                          `<span class="bg-[#ffc600]">${match}</span>`
+                      ),
+                    }}
+                  />
+                </li>
+              </Link>
+            ))
+          ) : (
+            <li className="bg-white list-none border-b-[1px] border-b-[#D8D8D8] m-0 p-[20px] transition-[background] flex justify-between capitalize">
+              Filter for an article
+            </li>
+          )}
         </ul>
       </form>
     </div>
--- src/pages/captainsLog/bigLogs/index.tsx
+++ src/pages/captainsLog/bigLogs/index.tsx
@@ -1,16 +1,50 @@
-import { GetServerSideProps } from "next";
+// import { GetServerSideProps } from "next";
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   // Redirect to /products if the user lands on /products/new-products
+//   return {
+//     redirect: {
+//       destination: "/captainsLog", // The target redirect URL
+//       permanent: false, // Use false for temporary redirect (HTTP 302), true for permanent (HTTP 308)
+//     },
+//   };
+// };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // Redirect to /products if the user lands on /products/new-products
+// // If you have a default export for this page, it can remain here, but it will not be rendered because of the redirect.
+// export default function bigLogs() {
+//   return null; // Optional: You can also display a placeholder if redirection somehow fails
+// }
+import ArticleFilter from "@/components/CaptainsLog/articleFilter";
+import { LogbookEntry } from "typings";
+import React from "react";
+import logbookEntry from "@/schemas/logbookEntry";
+import { fetchLogbookEntries } from "@/utils/fetchLogbookEntries";
+import dynamic from "next/dynamic";
+import { GetStaticProps } from "next";
+
+// type EntryCarouselProps = {
+//   children: React.ReactElement<{ _id: string }>[];
+
+// };
+type Props = {
+  logBookEntries: LogbookEntry[];
+};
+const bigLogs = ({ logBookEntries }: Props) => {
+  return (
+    <div className="relative flex flex-col justify-start items-center bg-gradient-to-br from-teal-300 to-teal-600 text-black w-screen h-screen overflow-y-scroll scrollbar-none overflow-x-clip">
+      <div className="w-full h-full">
+        <ArticleFilter logBookEntries={logBookEntries} />
+      </div>
+    </div>
+  );
+};
+export default dynamic(() => Promise.resolve(bigLogs), { ssr: false });
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const logBookEntries: LogbookEntry[] = await fetchLogbookEntries();
   return {
-    redirect: {
-      destination: "/captainsLog", // The target redirect URL
-      permanent: false, // Use false for temporary redirect (HTTP 302), true for permanent (HTTP 308)
+    props: {
+      logBookEntries,
     },
+    revalidate: 10,
   };
 };
-
-// If you have a default export for this page, it can remain here, but it will not be rendered because of the redirect.
-export default function bigLogs() {
-  return null; // Optional: You can also display a placeholder if redirection somehow fails
-}
